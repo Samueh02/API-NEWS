5d5148e8e686791a82b3597809d0f5d17d34be4e
Samuel Ortega <samujedi@gmail.com>
2024-10-02 17:10:11 +0200

    tabs2

--- news-api/src/app/services/news.service.ts
+++ news-api/src/app/services/news.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { NewsResp } from '../interfaces/news';
+import { map } from 'rxjs/operators';
+import { Article, NewsResp } from '../interfaces/news';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-    
+  private baseUrl = 'https://newsapi.org/v2/top-headlines';
   constructor(private http: HttpClient) {}
   getTopNews() {
     return this.http.get<NewsResp>(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=380edd757f3a49c18c0fb26364c57887`);
   }
+  getTopHeadlinesByCategory(category: string): Observable<Article[]> {
+    return this.http.get<NewsResp>(`${this.baseUrl}`, {
+      params: {
+        category,
+        apiKey: '380edd757f3a49c18c0fb26364c57887',
+        country: 'us' // Ajustar según necesidad
+      }
+    }).pipe(map(({ articles }) => articles));
+  }
+
 }
--- news-api/src/app/tab2/tab2.page.ts
+++ news-api/src/app/tab2/tab2.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service'; // Importa el servicio de noticias
+import { Article } from 'src/app/interfaces/news'; // Asegúrate de importar la interfaz Article
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+  // Definir el array de categorías
+  public categories: string[] = ['business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'];
+  // Definir los artículos que vamos a mostrar
+  public articles: Article[] = [];
+  // Categoría seleccionada por defecto
+  public selectedCategory: string = 'general';
 
-  constructor() {}
+  constructor(private newsService: NewsService) {}
 
+  // Método que se ejecuta al iniciar la página
+  ngOnInit() {
+    // Cargar noticias para la categoría seleccionada por defecto
+    this.getNewsByCategory(this.selectedCategory);
+  }
+
+  // Método para manejar el cambio de segmento
+  segmentChanged(event: any) {
+    this.selectedCategory = event.detail.value; // Actualizar la categoría seleccionada
+    this.getNewsByCategory(this.selectedCategory); // Cargar noticias para la nueva categoría
+  }
+
+  // Obtener noticias filtradas por categoría
+  getNewsByCategory(category: string) {
+    this.newsService.getTopHeadlinesByCategory(category).subscribe(articles => {
+      this.articles = articles; // Actualizar los artículos con las noticias filtradas
+    });
+  }
 }
